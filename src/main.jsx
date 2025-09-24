c25568583ed65b274d80a27a8a7ab5498533e9a4
Sumanta <sumanta2k16@gmail.com>
2025-09-24 23:19:50 +0530

    solved minified error 137

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,42 @@
+// import "./index.css";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { Toaster } from "sonner";
+
+
+// import App from "./App.jsx";
+// import { BrowserRouter } from "react-router-dom";
+
+// const queryClient = new QueryClient();
+
+// createRoot(document.getElementById("root")).render(
+//  <QueryClientProvider client={queryClient}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//       <Toaster richColors position="bottom-right" />
+//     </QueryClientProvider>
+// );
+
+
 import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
-
-
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
- <QueryClientProvider client={queryClient}>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
       <Toaster richColors position="bottom-right" />
     </QueryClientProvider>
+  </StrictMode>
 );
